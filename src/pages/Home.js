c93818fb2f9f66805886580bb3abd96019454c83
lincesites27 <contato@lincesites.com.br>
2023-01-30 16:10:03 -0300

    alteraçao no content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>HOME TESTE</div>
+    <div className='container'>
+
+    <div className="content-left">
+      <h1>Olá Maykon Melo seja Bem vindo</h1>
+      <p>Por aqui você vai ficar por dentro de todas as novidades da nossa empresa em primeira mão. 😜 
+
+Fique sempre atento(a) à nossa página e bom trabalho!</p>
+    </div>
+
+
+    <div className="content-right">
+      
+    </div>
+
+    </div>
   )
 }
 
